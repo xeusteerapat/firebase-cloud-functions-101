1edd860099b234a6a0372dec39c1831d774b7e00
xeusteerapat <xeus085@gmail.com>
2020-08-17 17:06:16 +0700

    create add request function

--- functions/index.js
+++ functions/index.js
@@ -21,3 +21,25 @@ exports.userDeleted = functions.auth.user().onDelete(user => {
   const doc = admin.firestore().collection('users').doc(user.uid);
   return doc.delete();
 });
+
+// http callable function (adding tutorial requests)
+exports.addRequest = functions.https.onCall((data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      'unauthenticated',
+      'Please login before add requests'
+    );
+  }
+
+  if (data.text.length > 30) {
+    throw new functions.https.HttpsError(
+      'invalid-argument',
+      'Request must be no more than 30 characters long'
+    );
+  }
+
+  return admin.firestore().collection('requests').add({
+    text: data.text,
+    upvotes: 0,
+  });
+});
